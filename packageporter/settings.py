ce5bdc7f07907b2421c4b4220a73b30995589b9c
elemc <me@elemc.name>
2016-10-27 17:30:25 +0300

    Change path to templates

--- packageporter/settings.py
+++ packageporter/settings.py
@@ -108,7 +108,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/pushrepo/packageporter/templates',
+    '/home/pushrepo/packageporter/packageporter/templates',
 )
 
 INSTALLED_APPS = (
