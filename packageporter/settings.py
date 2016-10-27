7f90f4be55fce39a89f6c95964d961c2673e6c16
elemc <me@elemc.name>
2016-10-27 17:53:20 +0300

    Changed load adminmedia to real path

--- packageporter/settings.py
+++ packageporter/settings.py
@@ -73,7 +73,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/pushrepo/packageporter/static/",
+    "/home/pushrepo/packageporter/packageporter/static/",
 )
 
 # List of finder classes that know how to find static files in
@@ -120,6 +120,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
+    'django.contrib.staticfiles.templatetags.adminmedia',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'packageporter.packages',
